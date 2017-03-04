2d2a853ae836299f42323c10a6c5333824aa6a65
Moises Linares <blade_corp@hotmail.com>
2017-03-04 16:24:51 -0600

    OK

--- lock/src/main/java/com/sysdt/lock/util/Security.java
+++ lock/src/main/java/com/sysdt/lock/util/Security.java
@@ -3,8 +3,8 @@ package com.sysdt.lock.util;
 public class Security {
 
 	public String convertirLlaves(String clave1, String clave2, int idCliente) throws Exception{
-//		return "BBBCCC";
-		return null;
+		return "BBBCCC";
+//		return null;
 	}
 	
 }
